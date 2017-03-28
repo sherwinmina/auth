98f0de00b2486d415d10d5bd59675e648632034f
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-27 23:14:18 -0700

    firebase set up

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp.apply({
+      apiKey: 'AIzaSyBaobC_znqrt8Sc8skhx_tqvTQX_MMpnY8',
+      authDomain: 'authentication-1d477.firebaseapp.com',
+      databaseURL: 'https://authentication-1d477.firebaseio.com',
+      storageBucket: 'authentication-1d477.appspot.com',
+      messagingSenderId: '1027350315105'
+    });
+  }
+
   render() {
     return (
       <View>
